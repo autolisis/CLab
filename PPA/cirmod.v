bb3bfa751f1f3d22d19d762e7a14711f30c08eea
Govind <kpgovind@gmail.com>
2017-03-24 12:58:54 +0530

    Cleanup

--- PPA/cirmod.v
+++
@@ -1,8 +0,0 @@
-module cirmod(gl , al , gr , ar , gout , aout);
-	input gr , ar , gl , al ;
-	output gout , aout;
-
-	assign gout = gl + (al & gr);
-	assign aout = al & ar;
-
-endmodule
--- PPA/dotmod.v
+++
@@ -1,7 +0,0 @@
-module dotmod(gl , al , gr , gout);
-	input gl , al , gr;
-	output gout;
-
-	assign gout = gl + (al & gr);
-
-endmodule
